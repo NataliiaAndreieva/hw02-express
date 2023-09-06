79ce17c7981da29ae640833107fe45e43a9a0521
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-06 10:32:51 +0300

    done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./routes/api/contacts');
 
-const app = express()
+const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
@@ -14,12 +14,13 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 
-app.use((req, res) => {
+app.use((_, res) => {
   res.status(404).json({ message: 'Not found' })
 })
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+app.use((err, _, res, __) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message })
 })
 
 module.exports = app
