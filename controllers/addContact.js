9d6e458e7905695b1de1a483e19a86b80cdebd9f
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-11 19:09:44 +0300

    add change

--- controllers/addContact.js
+++ controllers/addContact.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contact");
+const {Contact} = require("../models/contact");
 
 const addContact = async (req, res, _) => {
   const result = await Contact.create(req.body);
