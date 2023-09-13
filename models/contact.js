b63a57a2b85a0564275f0504c6577f824faacfa2
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-13 15:56:52 +0300

    some change

--- models/contact.js
+++ models/contact.js
@@ -37,13 +37,12 @@ const addSchema = Joi.object({
   favorite: Joi.boolean(),
 });
 
-const updateFavoriteSchema = Joi.object(
-  {
-    favorite: Joi.boolean().required().messages({
-      "any.required": "missing field favorite",
-    }),
-  }
-);
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "boolean.base": "Favorite should be a boolean",
+    "any.required": "missing field favorite",
+  }),
+});
 
 const Contact = model("contact", contactSchema);
 
