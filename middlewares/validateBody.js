687a74ddacce39d19417cb8c0ccd1f5cc46538c0
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-14 12:20:22 +0300

    PATCH

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -6,7 +6,7 @@ const validateBody = schema => {
         if (req.method === "PUT" && keys.length === 0) {
             return res.status(400).json({ message: 'Missing fields' });
         }
-        if (req.method === 'PUTCH' && keys.length === 0) {
+        if (req.method === 'PATCH' && keys.length === 0) {
             return res.status(400).json({ message: "missing field favorite" });
         }
         const { error } = schema.validate(req.body);
