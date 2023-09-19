273ad1aa3ec96f8536bb6f92ec40930a5bc60527
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-20 00:43:31 +0300

    avatar done

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use('/users', usersRouter);
 app.use("/api/contacts", contactsRouter);
@@ -27,6 +28,5 @@ app.use((err, _, res, __) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
 
 module.exports = app;
